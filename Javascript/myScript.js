3f678b6c078141304e2a4246a0e616919b4b5596
Kyrillos Kamal <kirllos1000@gmail.com>
2022-01-30 20:14:01 +0200

    Update myScript.js

--- Javascript/myScript.js
+++ Javascript/myScript.js
@@ -15,37 +15,71 @@ for (let i = 0; i < allImages.length; i++) {
 }
 
 closeBtn.addEventListener("click", function () {
-  lightBox.classList.replace("d-flex", "d-none");
+  close();
 });
+function close() {
+  lightBox.classList.replace("d-flex", "d-none");
+}
+document.addEventListener("keydown", function (e) {
+  if (e.code == "Escape") {
+    close();
+  }
+});
+// nextBtn.addEventListener("click", function () {
+//   currentIndex++;
+//   if (currentIndex == allImages.length) {
+//     currentIndex = 0;
+//   }
+//   var nextImageSlide = allImages[currentIndex].src;
+//   lightBoxImage.style.backgroundImage = `url(${nextImageSlide})`;
+// });
+
+// prevBtn.addEventListener("click", function () {
+//   currentIndex--;
+//   if (currentIndex < 0) {
+//     currentIndex = allImages.length - 1;
+//   }
+//   var nextImageSlide = allImages[currentIndex].src;
+//   lightBoxImage.style.backgroundImage = `url(${nextImageSlide})`;
+// });
 
 nextBtn.addEventListener("click", function () {
-  currentIndex++;
-  if (currentIndex == allImages.length) {
-    currentIndex = 0;
-  }
-  var nextImageSlide = allImages[currentIndex].src;
-  lightBoxImage.style.backgroundImage = `url(${nextImageSlide})`;
+  slide(1);
+});
+prevBtn.addEventListener("click", function () {
+  slide(-1);
 });
 
-prevBtn.addEventListener("click", function () {
-  currentIndex--;
+function slide(flag) {
+  currentIndex = currentIndex + flag;
   if (currentIndex < 0) {
     currentIndex = allImages.length - 1;
+  } else {
+    if (currentIndex == allImages.length) {
+      currentIndex = 0;
+    }
+    var nextImageSlide = allImages[currentIndex].src;
+    lightBoxImage.style.backgroundImage = `url(${nextImageSlide})`;
   }
-  var nextImageSlide = allImages[currentIndex].src;
-  lightBoxImage.style.backgroundImage = `url(${nextImageSlide})`;
-});
+}
 
 document.addEventListener("keydown", function (e) {
   if (e.target == "27") {
     lightBoxImage.style.backgroundImage = `url(${nextImageSlide})`;
   }
 });
-0;
+
 document.addEventListener("contextmenu", function (e) {
   e.preventDefault();
 });
+
 document.addEventListener("keydown", function (e) {
+  if (e.code == "ArrowRight") {
+    slide(1);
+  }
+  if (e.code == "ArrowLeft") {
+    slide(-1);
+  }
   if (e.code == "F12") {
     e.preventDefault();
   }
